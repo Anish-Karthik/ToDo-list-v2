6ec5293eebb1979089eb122376fb70d3be074bb5
Anish-Karthik <anishkarthik.54321@gmail.com>
2023-04-13 22:34:36 +0530

    add new delete routes and isdefaultList to response. Also use  ejs to direct to diff route in form

--- app.js
+++ app.js
@@ -5,17 +5,14 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/ToDoListDB');
 }
 main().catch(err => console.log("DB  linkage error"));
-
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 const itemSchema = new mongoose.Schema({
   name: {
@@ -23,20 +20,12 @@ const itemSchema = new mongoose.Schema({
     required: [true, "Please check your data entry, no name specified!"]
   }
 });
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemSchema]
+});
 const Item = mongoose.model("Item", itemSchema);
-
-const item1 = new Item({
-  name: "Welcome to your todolist!"
-});
-
-const item2 = new Item({
-  name: "Hit the + button to add a new item."
-});
-
-const item3 = new Item({
-  name: "<-- Hit this to delete an item."
-});
-const defaultItems = [item1, item2, item3];
+const List = mongoose.model("List", listSchema);
 
 
 app.get("/", async function(req, res) {
@@ -48,21 +37,29 @@ app.get("/", async function(req, res) {
     });
   }
   const day = date.getDate();
-  res.render("list", {listTitle: day, newListItems: foundItems});
+  res.render("list", {listTitle: day, newListItems: foundItems, defaultList: true});
 });
 
 app.post("/", async function(req, res){
   console.log(req.body);
+  const day = date.getDate();
+  const listName = req.body.list;
   const item = new Item({
     name: req.body.newItem
   });
-  item.save();
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
+  if(listName === day) {
+    item.save();
     res.redirect("/");
+  } else {
+    await List.findOne({name: listName}).exec().then((foundList) => {
+      if(foundList){
+        foundList.items.push(item);
+        foundList.save();
+      }else {
+        console.log("List not found");
+      }
+    });
+    res.redirect("/"+listName);
   }
 });
 
@@ -72,19 +69,49 @@ app.post("/delete", async function(req, res){
   res.redirect("/");
 });
 
+app.post("/delete/:listName", async function(req, res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.params.listName;
+  await List.findOne({name: listName}).exec().then((foundList) => {
+    console.log(foundList.items.length);
+    foundList.items.pull({_id: checkedItemId});
+    console.log(foundList.items.length);
+    foundList.save();
+  });
+  res.redirect("/"+listName);
+});
+
 app.get("/about", function(req, res){
   res.render("about");
 });
 
 app.get("/:listName", async function(req,res){
-  // const CustomItem = mongoose.model(req.params.listName, itemSchema);
-  
-  const customList = [];
-  console.log(req.params.listName);
-  res.render("list", {listTitle: req.params.listName + " list", newListItems: customList});
+  const customListName = req.params.listName;
+  await List.findOne({name: customListName}).then((foundList) => {
+    if(!foundList) {
+      const list = new List({
+        name: customListName,
+        items: defaultItems
+      });
+      list.save();
+      res.render("list", {listTitle: customListName, newListItems: defaultItems, defaultList: false});
+    } else {
+      res.render("list", {listTitle: foundList.name, newListItems: foundList.items, defaultList: false});
+    }
+  });
 });
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+const defaultItems = [item1, item2, item3];
